6fd2d4bdb0a2c6f213fd864c37d2042395a59478
Jordan Jones <jordan@Jordans-MacBook-Pro.local>
2025-06-27 19:28:37 -0500

    update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 WORKING_DIR=$PWD
+
+echo "Installing OhMyZSH"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+echo "Installing Brew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing Tmux"
+brew install tmux
+
 echo "Creating .vimrc symlink"
 ln -s $WORKING_DIR/.vimrc ~/.vimrc
+
+echo "Creating .tmux.conf symlink"
+ln -s $WORKING_DIR/.tmux.conf ~/.tmux.conf
+
+echo 
+ln -s $WORKING_DIR/.p10k.zsh ~/.p10k.zsh
 
 echo "Installing Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
